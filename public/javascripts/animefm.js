e9903e3882f5b2056db0bc054c13b8474705e13c
Asthriona <im@asthriona.com>
2019-12-02 13:06:38 +0100

    Update AnimeFM JS

--- public/javascripts/animefm.js
+++ public/javascripts/animefm.js
@@ -6,8 +6,8 @@
  * if you need some help go check our github we share some code there!
  * https://github.com/CloudsdaleGroup
  */
-console.log('%c Cloudsdale Radio', 'font-size:100px;color:#fff;text-shadow:0 3px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);')
-console.log('Version 5.2')
+console.log('%c AnimeFM', 'font-size:100px;color:#fff;text-shadow:0 3px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);')
+console.log('Version 1.0')
 console.log('Environement: Production')
 checkStation();
 setInterval(function (){
